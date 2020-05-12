a24d9b04c3c3ec0bed72556c49d1ec2921e08c23
James O'Halloran <james.p.ohalloran@gmail.com>
2020-05-12 10:02:58 -0300

    split out contentful-client to typescript

---
+++ components/react-tinacms-contentful/contentful-client.ts
@@ -0,0 +1,40 @@
+export class ContentfulClient {
+  proxy: string;
+  space: string;
+
+  constructor(space: string, proxy: string) {
+    this.space = space;
+    this.proxy = proxy;
+  }
+
+  async save(id: string, version: string, contentModel: string, fields: any) {
+    console.log("Saving entity: ", id, fields);
+    return this.req({
+      url: `https://api.contentful.com/spaces/${this.space}/environments/master/entries/${id}`,
+      method: "PUT",
+      headers: {
+        "X-Contentful-Content-Type": contentModel,
+        "X-Contentful-Version": version,
+        "Content-Type": "application/vnd.contentful.management.v1+json",
+      },
+      data: {
+        fields: fields,
+      },
+    }).then(() => window.location.reload());
+  }
+
+  async req(data: any) {
+    const response = await this.proxyRequest(data);
+    return response;
+  }
+
+  /**
+   * The methods below maybe don't belong on GitHub client, but it's fine for now.
+   */
+  proxyRequest(data: any) {
+    return fetch(this.proxy, {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+  }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,55 +1,14 @@
 import "../styles/index.css";
 import { TinaCMS, TinaProvider } from "tinacms";
-
-class ContentfulClient {
-  constructor({ space, accessToken, proxy }) {
-    this.space = space;
-    this.client = require("contentful-management").createClient({
-      accessToken,
-    });
-    this.proxy = proxy;
-  }
-
-  async save(id, version, contentModel, fields) {
-    console.log("Saving entity: ", id, fields);
-    return this.req({
-      url: `https://api.contentful.com/spaces/${this.space}/environments/master/entries/${id}`,
-      method: "PUT",
-      headers: {
-        "X-Contentful-Content-Type": contentModel,
-        "X-Contentful-Version": version,
-        "Content-Type": "application/vnd.contentful.management.v1+json",
-      },
-      data: {
-        fields: fields,
-      },
-    }).then(() => window.location.reload());
-  }
-
-  async req(data) {
-    const response = await this.proxyRequest(data);
-    return response;
-  }
-
-  /**
-   * The methods below maybe don't belong on GitHub client, but it's fine for now.
-   */
-  proxyRequest(data) {
-    return fetch(this.proxy, {
-      method: "POST",
-      body: JSON.stringify(data),
-    });
-  }
-}
+import { ContentfulClient } from "../components/react-tinacms-contentful/contentful-client";
 
 function MyApp({ Component, pageProps }) {
   const tinaConfig = {
     apis: {
-      contentful: new ContentfulClient({
-        space: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
-        proxy: "/api/proxy",
-      }),
+      contentful: new ContentfulClient(
+        process.env.CONTENTFUL_SPACE_ID,
+        "/api/proxy"
+      ),
     },
     sidebar: {
       hidden: false,
