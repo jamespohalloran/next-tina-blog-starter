3556099720879f2c041fe85dd5dae01e9cffc4d5
James O'Halloran <james.p.ohalloran@gmail.com>
2020-05-12 09:51:35 -0300

    make typescript use stricter types

--- components/next-tinacms-contentful/next/proxy.ts
+++ components/next-tinacms-contentful/next/proxy.ts
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-export const proxy = (req, res) => {
+export const proxy = (req: any, res: any) => {
   const { headers, ...data } = JSON.parse(req.body);
 
   axios({
@@ -10,10 +10,10 @@ export const proxy = (req, res) => {
       Authorization: "Bearer " + process.env.CONTENTFUL_MANAGEMENT_ACCESS_TOKEN,
     },
   })
-    .then((resp) => {
+    .then((resp: any) => {
       res.status(resp.status).json(resp.data);
     })
-    .catch((err) => {
+    .catch((err: any) => {
       res.status(err.response.status).json(err.response.data);
     });
 };
