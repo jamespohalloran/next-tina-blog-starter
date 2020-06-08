2bd1a2e4343edad8659899295287f92d7a8afa17
James O'Halloran <james.p.ohalloran@gmail.com>
2020-06-08 11:19:46 -0300

    helpers for debugging local tina & react-tinacms-contentful

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 require("dotenv").config();
+const path = require("path");
+
+const tinaWebpackHelpers = require("@tinacms/webpack-helpers");
 
 module.exports = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.resolve.alias["@tinacms/react-tinacms-contentful"] = path.resolve(
+        "../react-tinacms-contentful"
+      );
+      config.resolve.alias["react-beautiful-dnd"] = path.resolve(
+        "./node_modules/react-beautiful-dnd"
+      );
+
+      tinaWebpackHelpers.aliasTinaDev(config, "../tinacms");
+    }
+
+    return config;
+  },
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_DELIVERY_ACCESS_TOKEN:
